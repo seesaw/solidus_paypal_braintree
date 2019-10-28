f77838d19a9f564126a6024b0a618207c1030f82
Fabrizio Monti <fabrizio.monti@welaika.com>
2019-10-28 10:42:45 +0100

    Fix missing class loading in development
    Yaml.load tries to load an object of SolidusPaypalBraintree::Response
    class, but in development, if the class has never been used, it is not
    in memory and Yaml.load fails.
    
    Probably this errors does not happen in production. Anyway, I force the
    require of the class here in the gem.
    
    I hope this helps :(

--- lib/solidus_paypal_braintree.rb
+++ lib/solidus_paypal_braintree.rb
@@ -3,6 +3,8 @@ require 'solidus_paypal_braintree/engine'
 require 'solidus_paypal_braintree/country_mapper'
 require 'solidus_paypal_braintree/request_protection'
 
+require_relative '../app/models/solidus_paypal_braintree/response'
+
 module SolidusPaypalBraintree
   def self.table_name_prefix
     'solidus_paypal_braintree_'
